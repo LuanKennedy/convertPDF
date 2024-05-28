d1ab27ff3c82ade9075a85b8a64131453493f01e
xodoSoftware <luanc7459@gmail.com>
2024-05-28 14:34:37 -0300

    change structure for css running

--- app.py
+++ app.py
@@ -2,108 +2,86 @@ import logging
 import os
 from logging.handlers import RotatingFileHandler
 
-from flask import Flask, jsonify, request, send_from_directory
+from flask import Flask, jsonify, render_template, request, send_from_directory
 from PyPDF2 import PdfReader
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = './uploads'
-app.config['ALLOWED_EXTENSIONS'] = {'pdf'}
-app.config['LOG_FOLDER'] = './logs'
-app.config['SERVICE_TOKEN'] = 'SIMULATING_TOKEN'
+app.config['PASTA_UPLOAD'] = './uploads'
+app.config['EXTENSOES_PERMITIDAS'] = {'pdf'}
+app.config['PASTA_LOGS'] = './logs'
+app.config['TOKEN_SERVICO'] = 'SIMULANDO_TOKEN'
 
-if not os.path.exists(app.config['LOG_FOLDER']):
-    os.makedirs(app.config['LOG_FOLDER'])
+if not os.path.exists(app.config['PASTA_LOGS']):
+    os.makedirs(app.config['PASTA_LOGS'])
 
-log_handler = RotatingFileHandler(os.path.join(app.config['LOG_FOLDER'], 'app.log'), maxBytes=10000, backupCount=1)
+log_handler = RotatingFileHandler(os.path.join(app.config['PASTA_LOGS'], 'app.log'), maxBytes=10000, backupCount=1)
 log_handler.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 log_handler.setFormatter(formatter)
 app.logger.addHandler(log_handler)
 app.logger.setLevel(logging.INFO)
 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+def arquivo_permitido(nome_arquivo):
+    return '.' in nome_arquivo and nome_arquivo.rsplit('.', 1)[1].lower() in app.config['EXTENSOES_PERMITIDAS']
 
-def pdf_to_text(pdf_path):
+def pdf_para_texto(caminho_pdf):
     try:
-        reader = PdfReader(pdf_path)
-        text = ''
-        for page in reader.pages:
-            text += page.extract_text() or ''
-        return text
+        leitor = PdfReader(caminho_pdf)
+        texto = ''
+        for pagina in leitor.pages:
+            texto += pagina.extract_text() or ''
+        return texto
     except Exception as e:
-        return f"Error: {str(e)}"
+        return f"Erro: {str(e)}"
 
-def log_message(message):
-    app.logger.info(message)
+def registra_log(mensagem):
+    app.logger.info(mensagem)
 
 @app.route('/convert/pdf2txt', methods=['POST'])
 def convert_pdf_to_txt():
     if 'file' not in request.files:
-        log_message('No file sent')
+        registra_log('No file sent')
         return jsonify({'error': 'No file sent'}), 400
 
-    file = request.files['file']
-    if file.filename == '':
-        log_message('No selected file')
-        return jsonify({'error': 'No selected file'}), 400
+    arquivo = request.files['file']
+    if arquivo.filename == '':
+        registra_log('No file selected')
+        return jsonify({'error': 'No file selected'}), 400
 
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        pdf_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-        file.save(pdf_path)
+    if arquivo and arquivo_permitido(arquivo.filename):
+        nome_arquivo = secure_filename(arquivo.filename)
+        caminho_pdf = os.path.join(app.config['PASTA_UPLOAD'], nome_arquivo)
+        os.makedirs(app.config['PASTA_UPLOAD'], exist_ok=True)
+        arquivo.save(caminho_pdf)
 
-        text = pdf_to_text(pdf_path)
-        if text.startswith("Error"):
-            log_message(f'Error converting file {filename}: {text}')
-            return jsonify({'error': text}), 500
+        texto = pdf_para_texto(caminho_pdf)
+        if texto.startswith("Erro"):
+            registra_log(f'Error converting file {nome_arquivo}: {texto}')
+            return jsonify({'error': texto}), 500
 
-        txt_filename = filename.rsplit('.', 1)[0] + '.txt'
-        txt_path = os.path.join(app.config['UPLOAD_FOLDER'], txt_filename)
-        with open(txt_path, 'w', encoding='utf-8') as txt_file:
-            txt_file.write(text)
+        nome_txt = nome_arquivo.rsplit('.', 1)[0] + '.txt'
+        caminho_txt = os.path.join(app.config['PASTA_UPLOAD'], nome_txt)
+        with open(caminho_txt, 'w', encoding='utf-8') as arquivo_txt:
+            arquivo_txt.write(texto)
 
-        log_message(f'Conversion completed for file {filename}, saved as {txt_filename}')
+        registra_log(f'Conversion completed for file {nome_arquivo}, saved as {nome_txt}')
         return jsonify({
             'message': 'Conversion completed',
-            'txt_file': txt_filename,
-            'download_link': f'/download/{txt_filename}'
+            'txt_file': nome_txt,
+            'download_link': f'/download/{nome_txt}'
         }), 201
     else:
-        log_message(f'File not allowed: {file.filename}')
+        registra_log(f'File not allowed: {arquivo.filename}')
         return jsonify({'error': 'File not allowed'}), 400
 
 @app.route('/download/<filename>', methods=['GET'])
 def download_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-
-def decode_text(input_text):
-    try:
-        return input_text.decode('utf-8')
-    except UnicodeDecodeError as e:
-        app.logger.error(f"Error decoding: {e}")
-        return input_text.decode('utf-8', errors='ignore')
-
-@app.route('/generate-report', methods=['POST'])
-def generate_report():
-    token = request.headers.get('Authorization')
-    if token != app.config['SERVICE_TOKEN']:
-        log_message('Unauthorized access to log service')
-        return jsonify({'error': 'Unauthorized access'}), 403
-    try:
-        with open(os.path.join(app.config['LOG_FOLDER'], 'app.log'), 'rb') as log_file:  # Note the 'rb' mode
-            log_content = log_file.read()
-        decoded_log_content = decode_text(log_content)
-        return jsonify({'report': decoded_log_content}), 200
-    except Exception as e:
-        app.logger.error(f'Error generating report: {str(e)}')
-        return jsonify({'error': 'Error generating report'}), 500
+    return send_from_directory(app.config['PASTA_UPLOAD'], filename)
 
 @app.route('/')
 def index():
-    return send_from_directory('.', 'index.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
